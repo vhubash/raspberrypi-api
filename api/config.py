10e22bee9c4294d3a82394226f551f057f208c79
Volodymyr Hubash <vhubash@gmail.com>
2021-12-07 05:04:05 +0200

    confog db update

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 from api import app
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = JAWSDB_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['JAWSDB_URL']
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
