c4ed9f8ebbd7922ad543a9fa978109828c566b06
Volodymyr Hubash <vhubash@gmail.com>
2021-12-07 04:27:14 +0200

    config db update

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 from api import app
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zrrva3zwz4q7dssa:h3gvuwsje6uugiue@d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/bajzsahu0ut67fla'
+app.config['SQLALCHEMY_DATABASE_URI'] = JAWSDB_URL
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
