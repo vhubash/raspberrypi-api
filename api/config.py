b7ce5d07e2e3d68043079ea1bbc0fe4d9bc1ebd3
Volodymyr Hubash <vhubash@gmail.com>
2021-12-07 05:46:22 +0200

    update db congfig

--- api/config.py
+++ api/config.py
@@ -1,5 +1,6 @@
+import os
 from api import app
 app.debug = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
